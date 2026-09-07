126e4e0c468daaacb4e550ed6ed32e52306edd1d
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-09-07 20:01:27 -0300

    env.d.ts tiny fix.

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,10 +4,7 @@ import React from 'react';
 import TinyServiceWorker from 'tiny-essentials/libs/router/TinyServiceWorker';
 
 declare global {
-  type ReactSetStateAction<Value = any> = [
-    Value, 
-    React.Dispatch<React.SetStateAction<Value>>
-  ];
+  type ReactSetStateAction<T> = [T, React.Dispatch<React.SetStateAction<T>>];
   interface Window {
     __TINY_PWA_MANIFEST__: Record<string, any>;
     swManager: TinyServiceWorker<"web-manager", "/sw.js">;
